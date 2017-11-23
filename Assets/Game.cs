e4ba7ebff296d076c17c7ff14f4f171272fcdb00
CaseyManning <kcgrind@gmail.com>
2017-11-23 13:06:12 -0800

    Bad Mars
    game is now set on an ugly mars

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -8,6 +8,8 @@ public class Game : MonoBehaviour {
 	GameObject[] ships;
 	GameObject[] cities;
 
+	public GameObject camera;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +17,18 @@ public class Game : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey("left")) {
+			camera.transform.position = new Vector3 (camera.transform.position.x - 1, camera.transform.position.y, camera.transform.position.z);
+		}
+		if (Input.GetKey("right")) {
+			camera.transform.position = new Vector3 (camera.transform.position.x + 1, camera.transform.position.y, camera.transform.position.z);
+		}
+		if (Input.GetKey("up")) {
+			camera.transform.position = new Vector3 (camera.transform.position.x, camera.transform.position.y, camera.transform.position.z + 1);
+		}
+		if (Input.GetKey("down")) {
+			camera.transform.position = new Vector3 (camera.transform.position.x, camera.transform.position.y, camera.transform.position.z - 1);
+		}
+
 	}
 }
